e39156b6d89a7d666087adf1afeaecac26c04cc4
Ian Noyes <ian@12southmusic.com>
2014-08-12 17:11:58 -0500

    no nav

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -9,13 +9,5 @@
       </button>
       <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
     </div>
-
-    <nav class="collapse navbar-collapse" role="navigation">
-      <?php
-        if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
-        endif;
-      ?>
-    </nav>
   </div>
 </header>
