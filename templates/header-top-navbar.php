ba9763285b4444f4206c2ac9a9884d8c06cef3ed
Ian Noyes <ian@12southmusic.com>
2014-08-13 12:42:06 -0500

    add logo

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src="/wp-content/themes/splash/assets/img/logo.png" class="img-responsive"></a>
     </div>
   </div>
 </header>
