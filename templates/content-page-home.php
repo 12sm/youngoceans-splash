9db190ad08199691412738a90c1c9159a280cf3e
Ian Noyes <ian@12southmusic.com>
2014-08-15 16:03:10 -0500

    img padding

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,6 +1,8 @@
 <div class="row">
 	<div class='col-sm-8 col-sm-offset-2'>
+		<div class="img-container">
 			<img src="/wp-content/themes/splash/assets/img/graphic.png" class="img-responsive center-block">
+		</div>
 		<h1 class="headline">PRE-ORDER THE NEW ALBUM MEOW</h1>
 		<div class="btn-container">
 			<a href="#" class="btn btn-click">Click Here</a>
