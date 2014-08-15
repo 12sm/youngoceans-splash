fed7878dddaf0da8952160008c9f01ea940b0e8c
Ian Noyes <ian@12southmusic.com>
2014-08-15 16:20:31 -0500

    change text

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -3,7 +3,7 @@
 		<div class="img-container">
 			<img src="/wp-content/themes/splash/assets/img/graphic.png" class="img-responsive center-block">
 		</div>
-		<h1 class="headline">PRE-ORDER THE NEW ALBUM MEOW</h1>
+		<h1 class="headline">PRE-ORDER THE ALBUM NOW</h1>
 		<div class="btn-container">
 			<a href="http://young-oceans.myshopify.com" target='_blank' class="btn btn-click">Band Store</a>
 			<a href="https://itunes.apple.com/us/artist/young-oceans/id513397875" target='_blank' class="btn btn-click">iTunes</a>
