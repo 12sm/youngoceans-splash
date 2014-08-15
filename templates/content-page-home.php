90665ab1335868a6323f8aff672b94be29deabb7
Ian Noyes <ian@12southmusic.com>
2014-08-15 16:02:07 -0500

    margin 0 auto

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,8 +1,6 @@
 <div class="row">
 	<div class='col-sm-8 col-sm-offset-2'>
-		<div class="fit-vids">
-			<img src="/wp-content/themes/splash/assets/img/graphic.png" class="img-responsive">
-		</div>
+			<img src="/wp-content/themes/splash/assets/img/graphic.png" class="img-responsive center-block">
 		<h1 class="headline">PRE-ORDER THE NEW ALBUM MEOW</h1>
 		<div class="btn-container">
 			<a href="#" class="btn btn-click">Click Here</a>
